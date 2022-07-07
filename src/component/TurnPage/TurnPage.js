7ea1e1f3e0167ee31aaee767b6a366b8e4e15520
CLOWN <27668380022@qq.com>
2022-07-08 01:47:06 +0800

    feat: 搜索框优化, 实现筛选

--- src/component/TurnPage/TurnPage.js
+++ src/component/TurnPage/TurnPage.js
@@ -4,13 +4,13 @@ import { doSearch } from '../../utils/searchEngine';
 
 export const TurnPage = (props) => {
 	// eslint-disable-next-line react/prop-types
-	const { total, setSearchResult, setGetToTal,setGetNowPage,setGetNowSize } = props;
+	const { total, setSearchResult, setGetToTal,setGetNowPage,setGetNowSize,selectSalary,selectJob,selectSchool } = props;
 	// eslint-disable-next-line react/prop-types
 	const TurnPageOnChange = (current, pageSize) => {
 		console.log(current, pageSize);
 		setGetNowPage(current);
 		setGetNowSize(pageSize);
-		doSearch(document.getElementById('inputArea').value, setSearchResult, setGetToTal, current, pageSize);
+		doSearch(document.getElementById('inputArea').value, setSearchResult, setGetToTal, current, pageSize,selectSalary,selectJob,selectSchool);
 	};
 
 	return (
--- src/utils/searchEngine.js
+++ src/utils/searchEngine.js
@@ -1,11 +1,14 @@
 import {addHistory} from './indexDb';
-export function doSearch(searchInput,setSearchResult,setGetToTal,page,pageSize){
+export function doSearch(searchInput,setSearchResult,setGetToTal,page,pageSize,selectSalary,selectJob,selectSchool) {
 	const searchMode = document.getElementById('modeButton').textContent === "职位信息" ? 1 : 2;
 	let body = JSON.stringify({
 		searchMode: searchMode,
 		searchInput: searchInput,
 		page: page===undefined?1:page,
-		pageSize: pageSize===undefined?10:pageSize
+		pageSize: pageSize===undefined?10:pageSize,
+		salary: selectSalary===undefined||selectSalary.length===0?['不限']:selectSalary,
+		job: selectJob===undefined||selectSalary.length===0?['不限']:selectJob,
+		school: selectSchool===undefined||selectSalary.length===0?['不限']:selectSchool
 	});
 	console.log(body);
 	if (!window.location.pathname.includes('input')) {
@@ -18,7 +21,7 @@ export function doSearch(searchInput,setSearchResult,setGetToTal,page,pageSize){
 		window.location.href='/InterviewExperience'+'/input='+searchInput;
 	}
 	//如果现在的input和url中的input不一样，则更新url
-	else if (window.location.pathname.includes('input')&&searchInput!==window.location.pathname.split('/')[window.location.pathname.split('/').length-1].split('=')[1]) {
+	else if (window.location.pathname.includes('input')&&searchInput!==decodeURIComponent(window.location.pathname.split('/')[window.location.pathname.split('/').length-1].split('=')[1])) {
 		window.location.href= window.location.pathname.split('/')[window.location.pathname.split('/').length-1].split('=')[0]+'='+searchInput;
 	}
 	else {
